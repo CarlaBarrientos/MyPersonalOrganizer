ec3cb684c22381c6193de2ab305d2273a587d6b2
Carly42 <carla.andrea.cbm@gmail.com>
2020-06-21 18:00:58 -0400

    Adding the description of Unit Tests.

---
+++ tests/unit/scheduling.spec.js
@@ -0,0 +1,12 @@
+import { assert } from "chai";
+import { mount, createLocalVue } from "@vue/test-utils";
+import Scheduling from "@/views/Scheduling.vue";
+
+describe("Scheduling appointmets/meetings module", () => {
+  it("Validate data when fields are empty.", () => {});
+  it("Validate data when all fields are filled.", () => {});
+  it("Validate that the begin/end hours are within the agenda's range of hours.", () => {});
+  it("Add a new appointment/meeting validating data.", () => {});
+  it("Update a scheduled appointment/meeting.", () => {});
+  it("Delete a scheduled appontment/meeting that is not scheduled for today.", () => {});
+});
