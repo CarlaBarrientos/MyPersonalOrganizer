42cda6c2958d68d02be582e40705e0f29f57f435
Carly42 <carla.andrea.cbm@gmail.com>
2020-06-21 18:30:23 -0400

    Implementing data validation UT.

---
+++ src/views/Scheduling.vue
@@ -0,0 +1,35 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "Scheduling",
+  data() {
+    return {
+      code: "",
+      name: "",
+      description: "",
+      date: "",
+      startHour: "",
+      endHour: "",
+      agendaId: "",
+      participants: []
+    };
+  },
+
+  methods: {
+    _validateData() {
+      return (
+        this.code !== "" &&
+        this.name !== "" &&
+        this.description !== "" &&
+        this.date !== "" &&
+        this.startHour !== "" &&
+        this.endHour !== "" &&
+        this.agendaId !== ""
+      );
+    }
+  }
+};
+</script>
--- tests/unit/scheduling.spec.js
+++ tests/unit/scheduling.spec.js
@@ -1,10 +1,27 @@
 import { assert } from "chai";
-import { mount, createLocalVue } from "@vue/test-utils";
+import { shallowMount } from "@vue/test-utils";
 import Scheduling from "@/views/Scheduling.vue";
 
 describe("Scheduling appointmets/meetings module", () => {
-  it("Validate data when fields are empty.", () => {});
-  it("Validate data when all fields are filled.", () => {});
+  it("Validate data when fields are empty.", () => {
+    const wrapper = shallowMount(Scheduling);
+    const invalidData = wrapper.vm._validateData();
+    assert.isFalse(invalidData);
+  });
+  it("Validate data when all fields are filled.", () => {
+    const wrapper = shallowMount(Scheduling);
+
+    wrapper.vm.$data.code = "1";
+    wrapper.vm.$data.name = "presentation";
+    wrapper.vm.$data.description = "final project presentation";
+    wrapper.vm.$data.date = "2020-06-21";
+    wrapper.vm.$data.startHour = "10:00";
+    wrapper.vm.$data.endHour = "11:00";
+    wrapper.vm.$data.agendaId = "1";
+
+    const validData = wrapper.vm._validateData();
+    assert.isTrue(validData);
+  });
   it("Validate that the begin/end hours are within the agenda's range of hours.", () => {});
   it("Add a new appointment/meeting validating data.", () => {});
   it("Update a scheduled appointment/meeting.", () => {});
