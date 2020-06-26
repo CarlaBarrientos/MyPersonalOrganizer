49a3e425ed199ea801d10c9b79abb11a751025f9
Rodrigo Heredia <rodrigoh1205@gmail.com>
2020-06-26 17:06:28 -0400

    Completed unit tests for posponed appointments

---
+++ tests/unit/mockPostponing.js
@@ -0,0 +1,51 @@
+export default {
+  scheduledAppointments: [
+    {
+      code: "sched-1",
+      name: "Dentist",
+      description: "I need to go to dentist",
+      date: "2020-06-24",
+      startHour: "10:00",
+      endHour: "11:00",
+      agendaId: "ANG-001",
+      participants: []
+    },
+    {
+      code: "sched-2",
+      name: "Dentist",
+      description: "I need to go to dentist",
+      date: "2020-06-24",
+      startHour: "12:00",
+      endHour: "15:00",
+      agendaId: "ANG-001",
+      participants: []
+    },
+    {
+      code: "sched-3",
+      name: "Dentist",
+      description: "I need to go to dentist",
+      date: "2020-06-24",
+      startHour: "14:00",
+      endHour: "15:00",
+      agendaId: "ANG-001",
+      participants: []
+    }
+  ],
+  postponedAppointments: [
+    {
+      code: "sched-4",
+      name: "Football",
+      description: "I need to go to the stadium"
+    },
+    {
+      code: "sched-5",
+      name: "Doctor",
+      description: "I need to go to the doctor"
+    },
+    {
+      code: "sched-6",
+      name: "University",
+      description: "I need to go to my university"
+    }
+  ]
+};
