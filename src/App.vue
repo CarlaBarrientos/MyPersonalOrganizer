99e01b3960531d062760ed5fd39042fb7cf10306
Carly42 <carla.andrea.cbm@gmail.com>
2020-06-27 19:56:02 -0400

    Adding NavbarMenu

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
   <v-app>
+    <NavbarMenu />
     <v-main class="content">
       <router-view />
     </v-main>
   </v-app>
 </template>
 <script>
+import NavbarMenu from "@/components/NavbarMenu";
 export default {
   name: "App",
-  components: {},
+  components: { NavbarMenu },
   data: () => ({
     //
   })
---
+++ src/components/NavbarMenu.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-spacer></v-spacer>
+      <v-toolbar-title class="text-capitalize grey--text"
+        >My Personal Organizer</v-toolbar-title
+      >
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" app temporary>
+      <v-list nav dense>
+        <v-list-item-group>
+          <v-list-item
+            v-for="link in links"
+            :key="link.text"
+            router
+            :to="link.route"
+          >
+            <v-list-item-title>{{ link.text }}</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+<script>
+export default {
+  name: "NavbarMenu",
+  data() {
+    return {
+      drawer: false,
+      links: [
+        { text: "Appointments", route: "/" },
+        { text: "Agendas", route: "/agendas" },
+        { text: "Scheduled", route: "/appointments" },
+        { text: "Recursive", route: "/recursive" },
+        { text: "Postponed", route: "/postponed" },
+        { text: "Participants", route: "/participants" }
+      ]
+    };
+  }
+};
+</script>
