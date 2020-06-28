29fc7593f7d34be68fe7fb0de45f3957227f81b9
AndresGB <andresgamboabaldi@gmail.com>
2020-06-27 21:57:28 -0400

    Nav Bar Styles

--- src/components/NavbarMenu.vue
+++ src/components/NavbarMenu.vue
@@ -1,23 +1,32 @@
 <template>
   <div>
-    <v-app-bar app>
+    <v-app-bar color="blue" app>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
       <v-spacer></v-spacer>
-      <v-toolbar-title class="text-capitalize grey--text"
+      <v-toolbar-title class="text-capitalize white--text"
         >My Personal Organizer</v-toolbar-title
       >
+      <v-icon x-large color="black">mdi-calendar-clock</v-icon>
     </v-app-bar>
 
-    <v-navigation-drawer v-model="drawer" app temporary>
+    <v-navigation-drawer
+      class="light-blue darken-4"
+      v-model="drawer"
+      app
+      temporary
+    >
       <v-list nav dense>
         <v-list-item-group>
           <v-list-item
             v-for="link in links"
             :key="link.text"
             router
+            color="white"
             :to="link.route"
+            :class="`pa-3 link ${link.text}`"
           >
             <v-list-item-title>{{ link.text }}</v-list-item-title>
+            <v-icon color="white">{{ link.icon }}</v-icon>
           </v-list-item>
         </v-list-item-group>
       </v-list>
@@ -31,14 +40,42 @@ export default {
     return {
       drawer: false,
       links: [
-        { text: "Appointments", route: "/" },
-        { text: "Agendas", route: "/agendas" },
-        { text: "Scheduled", route: "/appointments" },
-        { text: "Recursive", route: "/recursive" },
-        { text: "Postponed", route: "/postponed" },
-        { text: "Participants", route: "/participants" }
+        { text: "All Appointments", route: "/", icon: "mdi-alarm" },
+        { text: "Agendas", route: "/agendas", icon: "mdi-calendar-star" },
+        {
+          text: "Scheduled",
+          route: "/appointments",
+          icon: "mdi-calendar-check"
+        },
+        { text: "Recursive", route: "/recursive", icon: "mdi-calendar-month" },
+        { text: "Postponed", route: "/postponed", icon: "mdi-calendar-remove" },
+        {
+          text: "Participants",
+          route: "/participants",
+          icon: "mdi-account-supervisor"
+        }
       ]
     };
   }
 };
 </script>
+<style>
+.link.Appointments {
+  border-left: 4px solid #546e7a;
+}
+.link.Agendas {
+  border-left: 4px solid #ffab00;
+}
+.link.Scheduled {
+  border-left: 4px solid #ffb300;
+}
+.link.Recursive {
+  border-left: 4px solid #ffd54f;
+}
+.link.Postponed {
+  border-left: 4px solid #ffecb3;
+}
+.link.Participants {
+  border-left: 4px solid #fff8e1;
+}
+</style>
