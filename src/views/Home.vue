a1743667a7bc67a9122aeb300a27e1555f560c12
AndresGB <andresgamboabaldi@gmail.com>
2020-06-29 12:08:25 -0400

    Changes on Calendar View

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,28 +1,90 @@
 <template>
-  <v-row>
+  <v-row class="fill-height">
     <v-col>
-      <v-flex md3 class="pl-5">
-        <v-menu :close-on-content-click="true" width="60">
-          <template v-slot:activator="{ on }">
-            <v-text-field
-              :value="date"
-              label="Date"
-              prepend-icon="mdi-calendar-range"
-              v-on="on"
-            ></v-text-field>
-          </template>
-          <v-date-picker v-model="date"></v-date-picker>
-        </v-menu>
-      </v-flex>
-      <v-sheet height="auto">
+      <v-sheet height="64">
+        <v-toolbar flat color="white">
+          <v-btn outlined class="mr-4" color="grey darken-2" @click="setToday"
+            >Today</v-btn
+          >
+          <v-btn fab text small color="grey darken-2" @click="prev">
+            <v-icon small>mdi-chevron-left</v-icon>
+          </v-btn>
+          <v-btn fab text small color="grey darken-2" @click="next">
+            <v-icon small>mdi-chevron-right</v-icon>
+          </v-btn>
+          <v-toolbar-title v-if="$refs.calendar">{{
+            $refs.calendar.title
+          }}</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-menu bottom right>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn outlined color="grey darken-2" v-bind="attrs" v-on="on">
+                <span>{{ typeToLabel[type] }}</span>
+                <v-icon right>mdi-menu-down</v-icon>
+              </v-btn>
+            </template>
+            <v-list>
+              <v-list-item @click="type = 'day'">
+                <v-list-item-title>Day</v-list-item-title>
+              </v-list-item>
+              <v-list-item @click="type = 'week'">
+                <v-list-item-title>Week</v-list-item-title>
+              </v-list-item>
+              <v-list-item @click="type = 'month'">
+                <v-list-item-title>Month</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </v-toolbar>
+      </v-sheet>
+      <v-sheet height="800">
         <v-calendar
           ref="calendar"
-          :now="date"
-          :value="date"
-          :events="parseScheduled"
+          v-model="focus"
           color="primary"
-          type="week"
+          :events="parseEvents"
+          :event-color="getEventColor"
+          :type="type"
+          @click:event="showEvent"
+          @click:more="viewDay"
+          @click:date="viewDay"
         ></v-calendar>
+        <v-menu
+          v-model="selectedOpen"
+          :close-on-content-click="false"
+          :activator="selectedElement"
+          offset-x
+        >
+          <v-card class="mx-auto" max-width="344">
+            <v-card-title>
+              <div class="subheading">{{ selectedEvent.name }}</div>
+            </v-card-title>
+            <v-card-text>
+              <div class="grey--text">
+                Description: {{ selectedEvent.description }}
+              </div>
+              <div class="grey--text">
+                Start hour: {{ selectedEvent.startHour }}
+              </div>
+              <div class="grey--text">
+                End hour: {{ selectedEvent.endHour }}
+              </div>
+              <div class="grey--text">
+                Participants:
+              </div>
+              <div
+                v-for="participant in participantsOnAppointment(
+                  selectedEvent.code
+                )"
+                :key="participant.participantId"
+              >
+                <v-chip small outlined color="black" text-color="black">
+                  {{ participant.name }}
+                </v-chip>
+              </div>
+            </v-card-text>
+          </v-card>
+        </v-menu>
       </v-sheet>
     </v-col>
   </v-row>
@@ -34,35 +96,81 @@ export default {
   name: "Home",
   data() {
     return {
-      date: this.today()
+      focus: "",
+      type: "month",
+      typeToLabel: {
+        month: "Month",
+        week: "Week",
+        day: "Day"
+      },
+      selectedEvent: {},
+      selectedElement: null,
+      selectedOpen: false
     };
+  },
+  mounted() {
+    this.$refs.calendar.checkChange();
   },
   computed: {
     ...mapGetters(["getScheduledList"]),
     scheduled() {
       return this.getScheduledList;
     },
-    parseScheduled() {
-      let parseScheduled = [];
+    parseEvents() {
+      let parseEvents = [];
       this.scheduled.forEach(appointment => {
         let start = appointment.date + " " + appointment.startHour;
         let end = appointment.date + " " + appointment.endHour;
-        let name = appointment.name;
-        let parsed = { start: start, end: end, name: name };
-        parseScheduled.push(parsed);
+        let color = "green";
+        appointment.start = start;
+        appointment.end = end;
+        appointment.color = color;
+        parseEvents.push(appointment);
       });
-      return parseScheduled;
+      return parseEvents;
     }
   },
   methods: {
-    today() {
-      let today = new Date();
-      const dd = String(today.getDate()).padStart(2, "0");
-      const mm = String(today.getMonth() + 1).padStart(2, "0");
-      const yyyy = today.getFullYear();
+    participantsOnAppointment(appointmentCode) {
+      let foundParticipants = [];
+      this.scheduled.forEach(appointment => {
+        if (appointment.code === appointmentCode) {
+          foundParticipants = appointment.participants;
+        }
+      });
+      return foundParticipants;
+    },
+    viewDay({ date }) {
+      this.focus = date;
+      this.type = "day";
+    },
+    getEventColor(event) {
+      return event.color;
+    },
+    setToday() {
+      this.focus = "";
+    },
+    prev() {
+      this.$refs.calendar.prev();
+    },
+    next() {
+      this.$refs.calendar.next();
+    },
+    showEvent({ nativeEvent, event }) {
+      const open = () => {
+        this.selectedEvent = event;
+        this.selectedElement = nativeEvent.target;
+        setTimeout(() => (this.selectedOpen = true), 10);
+      };
 
-      const currentDate = `${yyyy}-${mm}-${dd}`;
-      return currentDate;
+      if (this.selectedOpen) {
+        this.selectedOpen = false;
+        setTimeout(open, 10);
+      } else {
+        open();
+      }
+
+      nativeEvent.stopPropagation();
     }
   }
 };
